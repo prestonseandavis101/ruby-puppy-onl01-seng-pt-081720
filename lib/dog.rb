a76160e49f9b5c75d3623bdf7147e9eae3e4d90d
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-09 01:07:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+class Dog
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save.all?
+    end
+    
+    def save
+      @@all << self
+    end
+  
+  
+  def name
+    @name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    all.clear
+end
+
+def self.print_all
+  all.each {|x| puts x.name}
+end
+  
+
+
+  
+  
+end
