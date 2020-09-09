fc61e640bc0b576d75b8fd4a2968a960e553c74e
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-09 01:17:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+class Dog #FOCUS ON DOT NOTATION, LOOK UP EXAMPLES
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    self.save #.save is the method call, which is doing something to the left side of the period.THIS IS IMPLICIT
+    end
+    
+    def save
+      @@all << self #this shovels self into the @@all class variable
+    end
+  
+  
+  def name
+    @name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    all.clear
+end
+
+def self.print_all
+  all.each {|x| puts x.name} #go back and understand iterations
+end
+  
+
+
+  
+  
+end
